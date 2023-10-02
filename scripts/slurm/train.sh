deb74d35b8e20818796540d5d3a4845f7b4a3123
Emma Rafkin <emmarafkin@gmail.com>
2023-10-01 22:28:07 -0400

    loaded python module

--- scripts/slurm/train.sh
+++ scripts/slurm/train.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=END,FAIL
 
 module load cuda/11.8
-#module load python3/3.6.8
+module load python3/3.6.8
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt

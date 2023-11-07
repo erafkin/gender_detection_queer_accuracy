fb929eb2dda0f0f8b423136ed3f6436ec723a0a2
Emma Rafkin <emmarafkin@gmail.com>
2023-11-06 19:11:13 -0500

    added slurm script for evaluate

---
+++ scripts/slurm/evaluate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name="emma_train_classifier"
+#SBATCH --nodes=2
+#SBATCH --output="%x.o%j"
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8G
+#SBATCH --mail-user=epr41@georgetown.edu
+#SBATCH --mail-type=END,FAIL
+
+module load cuda/11.8
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 ./scripts/evaluate_classifier.py
+
